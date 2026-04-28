2370059e61b21d8c590207b95b682d2c65bac24c
maynardchrist10 <maynardmaynard2005@gmail.com>
2026-04-28 18:49:37 +0800

    pt-5

---
+++ posttest_5/2509106114_Maynard Christian Tallu Lembang_pt-5.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+struct BukuNode {
+    string judulBuku;
+    BukuNode* next;
+
+    // Constructor
+    BukuNode(string judul) {
+        judulBuku = judul;
+        next = nullptr;
+    }
+};
+
+BukuNode* tambahBuku(BukuNode* head, string judul) {
+    BukuNode* newNode = new BukuNode(judul);
+    if (head == nullptr) {
+        newNode->next = newNode;
+        return newNode;
+    }
+    
+    BukuNode* temp = head;
+    while (temp->next != head) {
+        temp = temp->next;
+    }
+    temp->next = newNode;
+    newNode->next = head;
+    return head;
+}
+
+
+bool cariBuku(BukuNode* head, string judulDicari) {
+
+    // soal nomor 1
+    if (head == nullptr) {
+        return false;
+    }
+
+    // soal nomor 2
+    BukuNode* temp = head;
+
+    do {
+        // soal nomor 3
+        if (temp->judulBuku == judulDicari) {
+            return true;
+        }
+
+        // soal nomor 4
+        temp = temp->next;
+
+    } while (temp != head); 
+
+    // soal nomor 5
+    return false;
+}
+
+int main() {
+    BukuNode* head = nullptr;
+    
+    head = tambahBuku(head, "Struktur Data dengan C++");
+    head = tambahBuku(head, "Pengantar Algoritma");
+    head = tambahBuku(head, "Pemrograman Berorientasi Objek");
+    head = tambahBuku(head, "Sistem Basis Data");
+
+    string buku1 = "Pengantar Algoritma";
+    string buku2 = "Kalkulus Lanjut";
+
+    cout << "Mencari buku '" << buku1 << "': " 
+         << (cariBuku(head, buku1) ? "Ditemukan" : "Tidak Ditemukan") << endl;
+
+    cout << "Mencari buku '" << buku2 << "': " 
+         << (cariBuku(head, buku2) ? "Ditemukan" : "Tidak Ditemukan") << endl;
+
+    return 0;
+}
